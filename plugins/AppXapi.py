da8a8d549024061be58d5b378f1c7b65fadb065b
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-02-23 14:49:56 +0530

    Update AppXapi.py

--- plugins/AppXapi.py
+++ plugins/AppXapi.py
@@ -165,17 +165,17 @@ async def start(bot, m):
             for video in gg:
                 #video_title, video_link, pdf_link, pdf_link2 = (video["Title"]), decrypt(download_link.split(":")[0]), decrypt((video["pdf_link"]).split(":")[0]), decrypt((video["pdf_link2"]).split(":")[0])
                 video_title = video["Title"].replace(':', '')
-                video_id = video["video_id"]
+                #video_id = video["video_id"]
                 video_link = video["download_link"].replace('NceL4AYD', '3iwbS6ep').replace("-NceL4AYD", "-gg65VkH4")
                 pdf_link = video.get("pdf_link", "")
                 pdf_link2 = video.get("pdf_link2", "")
-                video_title, video_link, pdf_link, pdf_link2 = (video["Title"]), decrypt(video_link.split(":")[0]), decrypt((video["pdf_link"]).split(":")[0]), decrypt((video["pdf_link2"]).split(":")[0])
+                video_title, video_link, pdf_link, pdf_link2 = (video["Title"]), decrypt(video_link.split(":")[0]), decrypt(pdf_link.split(":")[0]), decrypt(pdf_link2.split(":")[0])
                 if pdf_link and pdf_link != video_link:
                      video_link += f"\n{subject_title} {video_title} (pdf):{pdf_link}" 
                 if pdf_link2:
                     video_link += f"\n{subject_title} {video_title} (pdf-2):{pdf_link2}"
                 elif not video_link:
-                     video_link = f"{video_id}"
+                     video_link = f"{video_link}"
                 output_dict[subject_title][video_title] = video_link
                 with open(f"{course_title}.txt", 'a') as f:
                     f.write(f"{subject_title} {video_title}:{video_link}\n")
