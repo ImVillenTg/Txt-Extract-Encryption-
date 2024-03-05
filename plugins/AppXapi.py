605bb875dd42365d3d33fe0c31b4e91eec70a570
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-03-06 00:29:58 +0530

    Update AppXapi.py

--- plugins/AppXapi.py
+++ plugins/AppXapi.py
@@ -174,8 +174,6 @@ async def start(bot, m):
                 if pdf_link2 and pdf_link2 != pdf_link:
                     pdf_link2_decrypted = decrypt(pdf_link2.split(":")[0])
                     video_link += f"\n{subject_title} {video_title} (pdf-2):{pdf_link2_decrypted}"
-                elif not video_link:
-                     video_link = f"{video_link}"
                 output_dict[subject_title][video_title] = video_link
                 with open(f"{course_title}.txt", 'a') as f:
                     f.write(f"{subject_title} {video_title}:{video_link}\n")
