74977cb803c6f8caf4abcd1bbbaf25f9fde46625
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-03-05 23:19:02 +0530

    Update AppXapi.py

--- plugins/AppXapi.py
+++ plugins/AppXapi.py
@@ -161,14 +161,12 @@ async def start(bot, m):
             for video in gg:
                 video_title = video["Title"].replace(':', '')
                 video_link = decrypt((video["download_link"]).split(":")[0])
-                pdf_link = video.get("pdf_link", "")
-                pdf_link2 = video.get("pdf_link2", "")                                                                                   
+                pdf_link = decrypt((video["pdf_link"]).split(":")[0])
+                pdf_link2 = decrypt((video["pdf_link2"]).split(":")[0])                                                                                   
                 if pdf_link and pdf_link != video_link:
-                    pdf_link_decrypted = decrypt(pdf_link.split(":")[0])
-                    video_link += f"\n{subject_title} {video_title} (pdf):{pdf_link_decrypted}" 
+                    video_link += f"\n{subject_title} {video_title} (pdf):{pdf_link}" 
                 if pdf_link2:
-                    pdf_link2_decrypted = decrypt(pdf_link2.split(":")[0])
-                    video_link += f"\n{subject_title} {video_title} (pdf-2):{pdf_link2_decrypted}"
+                    video_link += f"\n{subject_title} {video_title} (pdf-2):{pdf_link2}"
                 output_dict[subject_title][video_title] = video_link
                 with open(f"{course_title}.txt", 'a') as f:
                     f.write(f"{subject_title} {video_title}:{video_link}\n")
