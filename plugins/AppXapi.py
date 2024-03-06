2cc069aff0088cb061f3a3846536a45a3bc03be5
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-03-06 08:02:51 +0530

    Update AppXapi.py

--- plugins/AppXapi.py
+++ plugins/AppXapi.py
@@ -164,14 +164,13 @@ async def start(bot, m):
             gg = output5["data"]
             for video in gg:
                 video_title = video["Title"].replace(':', '')
-                #video_id = video["video_id"]
                 video_link = decrypt((video["download_link"]).split(":")[0])
                 pdf_link = video.get("pdf_link", "")
                 pdf_link2 = video.get("pdf_link2", "")                                                                                   
                 if pdf_link and pdf_link != video_link:
                     pdf_link_decrypted = decrypt(pdf_link.split(":")[0])
                     video_link += f"\n{subject_title} {video_title} (pdf):{pdf_link_decrypted}" 
-                if pdf_link2 and pdf_link2 != pdf_link:
+                if pdf_link2:
                     pdf_link2_decrypted = decrypt(pdf_link2.split(":")[0])
                     video_link += f"\n{subject_title} {video_title} (pdf-2):{pdf_link2_decrypted}"
                 output_dict[subject_title][video_title] = video_link
