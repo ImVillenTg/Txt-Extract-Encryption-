211abb9583a596e8178e0fcc2eb7cae69e5923c8
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-03-06 14:21:45 +0530

    Update AppXapi.py

--- plugins/AppXapi.py
+++ plugins/AppXapi.py
@@ -168,9 +168,9 @@ async def start(bot, m):
                 video_link = decrypt((video["download_link"]).split(":")[0])
                 pdf_link = video.get("pdf_link", "")
                 pdf_link2 = video.get("pdf_link2", "")                                                                                   
-                if pdf_link and pdf_link != video_link:
-                    pdf_link_decrypted = decrypt(pdf_link.split(":")[0])
-                    video_link += f"\n{subject_title} {video_title} (pdf):{pdf_link_decrypted}" 
+                #if pdf_link and pdf_link != video_link:
+                    #pdf_link_decrypted = decrypt(pdf_link.split(":")[0])
+                    #video_link += f"\n{subject_title} {video_title} (pdf):{pdf_link_decrypted}" 
                 if pdf_link2:
                     pdf_link2_decrypted = decrypt(pdf_link2.split(":")[0])
                     video_link += f"\n{subject_title} {video_title} (pdf-2):{pdf_link2_decrypted}"
