329559fabf5cff7e7ee52e7d6a832ae5678db914
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-05-19 08:40:22 +0530

    Update Appx2Api.py

--- plugins/Appx2Api.py
+++ plugins/Appx2Api.py
@@ -25,8 +25,8 @@ from Crypto.Cipher import AES
 
 import datetime
 
-#time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-time = datetime.datetime.now().strftime("%d-%m-%Y %I:%M:%p")
+
+time = datetime.datetime.now().strftime("%d-%m-%Y")
 
 my_data = -1001938939742
 sudo_group = config.GROUPS
