068bb4a379cec22315b843f153c5a5345ef8fe57
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-08-03 09:10:07 +0530

    Update Appx.py

--- plugins/Appx.py
+++ plugins/Appx.py
@@ -157,53 +157,47 @@ async def start(bot, m):
             output5 = json.loads(html5)
             gg = output5["data"]
             for video in gg:
-            	if video.get("download_link"):
-                        video_title = video["Title"].replace(':', '')
-                        fuck = video["download_link"]
-                        video_link = decrypt((fuck).split(":")[0])
-                        pdf_link = video["pdf_link"]
-                        pdf_link2 = video["pdf_link2"]
-                        if pdf_link and pdf_link != fuck:
-                            pdf_link_decrypted = decrypt(pdf_link.split(":")[0])
-                            video_link += f"\n({subject_title}) {video_title} PDF:{pdf_link_decrypted}"
-                            total_links += 1
-                        if pdf_link2:
-                            pdf_link2_decrypted = decrypt(pdf_link2.split(":")[0])
-                            video_link += f"\n({subject_title}) {video_title} PDF-2:{pdf_link2_decrypted}"
-                            total_links += 1
-                        with open(f"{course_title}.txt", 'a') as f:
-                            f.write(f"({subject_title}) {video_title.replace('||', '').replace('#', '')}:{video_link}\n")
-                            total_links += 1
-                    else:
-                        video_id = video["id"]
-                        video_title = video["Title"].replace(':', '')
-                        scraper = cloudscraper.create_scraper()            
-                        html6 = scraper.get("https://"+raw_text05+"/get/fetchVideoDetailsById?course_id=" + raw_text1 + "&video_id=" + video_id + "&ytflag=0&folder_wise_course=0", headers=hdr).content
-                        output6 = json.loads(html6)       
-                        #gg = output6['data']['download_link']
-                        gg = output6['data'].get('download_link')
-                        if gg:
-                            video_link = decrypt((gg).split(":")[0])
-                        else:
-                            video_link = "None"
+                if video.get("download_link"):
+                    video_title = video["Title"].replace(':', '')
+                    fuck = video["download_link"]
+                    video_link = decrypt((fuck).split(":")[0])
+                    pdf_link = video["pdf_link"]
+                    pdf_link2 = video["pdf_link2"]
+                    if pdf_link and pdf_link != fuck:
+                        pdf_link_decrypted = decrypt(pdf_link.split(":")[0])
+                        video_link += f"\n({subject_title}) {video_title} PDF:{pdf_link_decrypted}"
+                        total_links += 1
+                    if pdf_link2:
+                        pdf_link2_decrypted = decrypt(pdf_link2.split(":")[0])
+                        video_link += f"\n({subject_title}) {video_title} PDF-2:{pdf_link2_decrypted}"
+                        total_links += 1
+                    with open(f"{course_title}.txt", 'a') as f:
+                        f.write(f"({subject_title}) {video_title.replace('||', '').replace('#', '')}:{video_link}\n")
+                        total_links += 1
+                else:
+                    video_id = video["id"]
+                    video_title = video["Title"].replace(':', '')
+                    scraper = cloudscraper.create_scraper()            
+                    html6 = scraper.get("https://"+raw_text05+"/get/fetchVideoDetailsById?course_id=" + raw_text1 + "&video_id=" + video_id + "&ytflag=0&folder_wise_course=0", headers=hdr).content
+                    output6 = json.loads(html6)       
+                    gg = output6['data'].get('download_link')
+                    if gg:
                         video_link = decrypt((gg).split(":")[0])
-                        pdf_lk = output6['data']["pdf_link"]
-                        pdf_lk2 = output6['data']["pdf_link2"]
-                        if pdf_lk:
-                            pdf_link_decrypted = decrypt(pdf_lk.split(":")[0])
-                            video_link += f"\n({subject_title}) {video_title} PDF:{pdf_link_decrypted}"
-                            total_links += 1
-                        else:
-                            pdf_link_decrypted = "None"
-                        if pdf_lk2:
-                            pdf_link2_decrypted = decrypt(pdf_lk2.split(":")[0])
-                            video_link += f"\n({subject_title}) {video_title} PDF-2:{pdf_link2_decrypted}"
-                            total_links += 1
-                        else:
-                            pdf_link2_decrypted = "None"
-                        with open(f"{course_title}.txt", 'a') as f:
-                            f.write(f"({subject_title}) {video_title.replace('||', '').replace('#', '')}:{video_link}\n")
-                            total_links += 1
+                    else:
+                        video_link = "None"
+                    pdf_lk = output6['data']["pdf_link"]
+                    pdf_lk2 = output6['data']["pdf_link2"]
+                    if pdf_lk:
+                        pdf_link_decrypted = decrypt(pdf_lk.split(":")[0])
+                        video_link += f"\n({subject_title}) {video_title} PDF:{pdf_link_decrypted}"
+                        total_links += 1
+                    if pdf_lk2:
+                        pdf_link2_decrypted = decrypt(pdf_lk2.split(":")[0])
+                        video_link += f"\n({subject_title}) {video_title} PDF-2:{pdf_link_decrypted}"
+                        
+                    with open(f"{course_title}.txt", 'a') as f:
+                        f.write(f"({subject_title}) {video_title.replace('||', '').replace('#', '')}:{video_link}\n")
+                        total_links += 1
  
     caption_details = raw_text05.replace("api.classx.co.in", "").replace("api.teachx.co.in", "").replace("api.appx.co.in", "").replace("apinew.teachx.in", "").replace ("api.akamai.net.in", "").replace("api.teachx.in", "").upper()
     file1 = InputMediaDocument(f"{course_title}.txt", caption=f"**🌀 Batch Id :** {raw_text1}\n\n**✳️ App :** {caption_details} (AppX V1)\n\n**📚 Batch :** `{course_title}`\n\n**🔰 Total Links :** {total_links}\n\n**🌪️ Thumb :** `{batch_logo}`\n\n**❄️ Date :** {time}")
