32cf01846b45db5ac4282e0b9c9978de8f77d21e
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-09-05 09:30:40 +0530

    Update Appx.py

--- plugins/Appx.py
+++ plugins/Appx.py
@@ -184,8 +184,8 @@ async def start(bot, m):
                     try:
                         await editable.edit(f"**ᴇxᴛʀᴀᴄᴛɪɴɢ ᴅᴀᴛᴀ ꜰʀᴏᴍ {subject_title} ➠ {video_title}\n\nᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴘᴀᴛɪᴇɴᴛʟʏ** ⚙️")
                     except FloodWait as e:
-                        print(f"Waiting for {e.x} seconds before continuing")
-                        await asyncio.sleep(e.x)
+                        print(f"Waiting for {e.value} seconds before continuing")
+                        await asyncio.sleep(e.value)
                         await editable.edit(f"**ᴇxᴛʀᴀᴄᴛɪɴɢ ᴅᴀᴛᴀ ꜰʀᴏᴍ {subject_title} ➠ {video_title}\n\nᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴘᴀᴛɪᴇɴᴛʟʏ** ⚙️")
                     scraper = cloudscraper.create_scraper()            
                     html6 = scraper.get("https://"+raw_text05+"/get/fetchVideoDetailsById?course_id=" + raw_text1 + "&video_id=" + video_id + "&ytflag=0&folder_wise_course=0", headers=hdr).content
