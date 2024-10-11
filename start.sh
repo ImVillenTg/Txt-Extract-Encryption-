4d5908031019c9c9be64b962f57418dfa2d2da23
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-10-11 19:06:03 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo..."
+git clone https://github.com/ImVillenTg/Txt-Extract-Encryption-
+cd /Txt-Extract-Encryption-
+pip install -r requirements.txt
+echo "Starting Bot..."
+python main.py
