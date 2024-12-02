1023c6c678fac758781472a8662f214fbfb9dd15
ImVillenTg <152398659+ImVillenTg@users.noreply.github.com>
2024-12-03 00:02:09 +0530

    Update Appx.py

--- plugins/Appx.py
+++ plugins/Appx.py
@@ -151,6 +151,7 @@ async def start(bot, m):
             #tsids_list.append(tsids)
         #for tsids in tsids_list:
             response5 = requests.get(f"https://"+raw_text05+"/get/livecourseclassbycoursesubtopconceptapiv3?topicid=" + tsids + "&start=-1&courseid=" + raw_text1 + "&subjectid=" + tids, headers=hdr).json()
+            rexop = ""
             for i in range(len(response5["data"])):
                 video_title = response5["data"][i]["Title"].strip() #.replace('||', '').replace('#', '').replace(':', '').replace(',', '').replace('@', '').replace('|', '')
                 video_link = response5["data"][i]["download_link"]
